5da1b3cc5034659bbb8d731b17a35a9fd1ade5c5
strxpro <claudiotaras3@gmail.com>
2025-06-06 17:17:43 +0200

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,8 @@
 import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { useFrameworkReady } from '../hooks/useFrameworkReady';
+import { useFrameworkReady } from '../hooks/useFrameworkReady'
+import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 export default function RootLayout() {
   useFrameworkReady();
